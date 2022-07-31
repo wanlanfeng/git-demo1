f94403126eb70b6c9ceca8db6b95eebee62fd061
wanlanfeng <2200198962@qq.com>
2022-07-31 08:21:40 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,11 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello, git!!!");
         System.out.println("hello, git!!!");
+        System.out.println("hello, git!!!");
+        System.out.println("hello, git!!!");
+        System.out.println("hello, git!!!");
+        System.out.println("hello, git!!!");
+        System.out.println("hello, git!!!");
+        System.out.println("hello, git!!!");
     }
 }
